16c7de2819152cf3e9e5ef0a221fb3f5f0c6eb9b
beans-squared <starskillz13@gmail.com>
2022-06-03 12:45:24 -0500

    Added some quick and dirty truncation for long project tracking lists

--- commands/list.js
+++ commands/list.js
@@ -6,6 +6,7 @@ module.exports = {
 		.setName('list')
 		.setDescription('Get a list of all the projects currently in tracking.'),
 	async execute(interaction) {
+		await interaction.deferReply();
 		const projects = await Projects.findAll({
 			where: {
 				guild_id: interaction.guild.id,
@@ -17,8 +18,11 @@ module.exports = {
 			list += `**Title:** ${inlineCode(project.project_title)} | **ID:** ${inlineCode(project.project_id)} | **Updates Channel:** ${interaction.guild.channels.cache.find(element => element.id === project.post_channel)}\n`;
 		}
 
+		const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
+		list = trim(list, 1900);
+
 		if (list === '') return await interaction.reply(`No projects are currently in tracking. Add some by using the ${inlineCode('/track')} command or by clicking the "Track Project" button when using the ${inlineCode('/search')} command.`);
 
-		await interaction.reply(`List of Modrinth projects currently in tracking:\n${list}`);
+		await interaction.editReply(`List of Modrinth projects currently in tracking:\n${list}`);
 	},
 };
