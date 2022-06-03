46ee5eb2001c3876b3a4c24bff4ffabb5602d343
beans-squared <starskillz13@gmail.com>
2022-06-03 12:21:48 -0500

    Modified the 'track project' button on search modals to be always usable

--- commands/search.js
+++ commands/search.js
@@ -1,10 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageActionRow, MessageButton, Permissions } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const { request } = require('undici');
 const dayjs = require('dayjs');
-const { trackProject } = require('./track');
 const { getJSONResponse } = require('./../util/getJSONResponse');
-const { verifyMemberPermission } = require('../util/verifyPermissions');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -43,13 +41,8 @@ module.exports = {
 				{ name: 'Project ID', value: `${hits[0].project_id}` },
 			);
 		const trackButton = new MessageButton()
-			.setCustomId('track:' + hits[0].project_id)
+			.setCustomId(`track:${hits[0].project_id}`)
 			.setLabel('Track Project')
-			.setStyle('PRIMARY');
-		const disabledTrackButton = new MessageButton()
-			.setCustomId('reeeeeeeeeeeeeeeeeeeeee')
-			.setLabel('Project Tracked')
-			.setDisabled()
 			.setStyle('PRIMARY');
 		const viewButton = new MessageButton()
 			.setURL(`https://modrinth.com/${hits[0].project_type}/${hits[0].slug}`)
@@ -58,29 +51,5 @@ module.exports = {
 		const row = new MessageActionRow().addComponents(trackButton, viewButton);
 
 		await interaction.editReply({ embeds: [ embed ], components: [ row ] });
-
-		const filter = i => i.customId.startsWith('track');
-		const collector = interaction.channel.createMessageComponentCollector({ filter, time: 30000 });
-		console.log('Started new button collector.');
-
-		const usedRow = new MessageActionRow().addComponents(disabledTrackButton, viewButton);
-		const timedOutRow = new MessageActionRow().addComponents(viewButton);
-
-		async function removeTrackButton() {
-			await interaction.editReply({ components: [ timedOutRow ] });
-		}
-		setTimeout(removeTrackButton, 30000);
-
-		collector.on('collect', async i => {
-			if (i.customId.startsWith('track')) {
-				if (!verifyMemberPermission(Permissions.FLAGS.MANAGE_CHANNELS, interaction.member)) return await i.reply({ content: 'You can only add projects to tracking if you have the \'Manage Channels\' permission.', ephemeral: true });
-				await i.update({ content: 'Project Tracked', components: [ usedRow ] });
-				trackProject(interaction, interaction.channel, hits[0].project_id);
-			}
-		});
-		collector.on('end', collected => {
-			console.log(`Collection ended. Size: ${collected.size}`);
-		});
-
 	},
 };
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,10 @@
+const { verifyMemberPermission } = require('../util/verifyPermissions');
+const { Permissions } = require('discord.js');
+const { trackProject } = require('../commands/track');
+
 module.exports = {
 	name: 'interactionCreate',
-	execute(interaction) {
+	async execute(interaction) {
 		// Slash command interactions
 		if (interaction.isCommand()) {
 			const command = interaction.client.commands.get(interaction.commandName);
@@ -11,10 +15,16 @@ module.exports = {
 				command.execute(interaction);
 			} catch (error) {
 				console.error(error);
-				interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
+				await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
 			}
 		} else if (interaction.isButton()) {
-			0;
+			if (interaction.customId.startsWith('track:')) {
+				if (!verifyMemberPermission(Permissions.FLAGS.MANAGE_CHANNELS, interaction.member)) return await interaction.reply({ content: 'You can only add projects to tracking if you have the \'Manage Channels\' permission.', ephemeral: true });
+
+				const projectId = interaction.customId.substring(6);
+				await interaction.deferReply();
+				trackProject(interaction, interaction.guild.channels.cache.find(element => element.id === interaction.channel.id), projectId);
+			}
 		}
 	},
 };
