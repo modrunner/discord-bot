a2cc80f426456f2ae8c8b48e69089777a6a8d081
beans-squared <starskillz13@gmail.com>
2022-07-12 21:15:38 -0500

    Potential fix for TypeError on project update check

--- util/checkForProjectUpdates.js
+++ util/checkForProjectUpdates.js
@@ -17,32 +17,6 @@ module.exports = {
 
 		let isUpdateFound = false;
 
-		async function fetchCurseForgeProject(project, fetchAttempts) {
-			if (fetchAttempts > 3) {
-				const channel = client.channels.cache.find(element => element.id === project.post_channel);
-				channel.send(`⚠️ An error has occured while requesting data on ${project.project_title} (${project.project_id}) from CurseForge.\nIf this issue persists, please contact the developer of this application.`);
-				return null;
-			}
-
-			const responseData = await getMod(project.project_id);
-			fetchAttempts++;
-			if (!responseData) await fetchCurseForgeProject(project, fetchAttempts);
-			return responseData;
-		}
-
-		async function fetchModrinthProject(project, fetchAttempts) {
-			if (fetchAttempts > 3) {
-				const channel = client.channels.cache.find(element => element.id === project.post_channel);
-				channel.send(`⚠️ An error has occured while requesting data on ${project.project_title} (${project.project_id}) from Modrinth.\nIf this issue persists, please contact the developer of this application.`);
-				return null;
-			}
-
-			const responseData = await getProject(project.project_id);
-			fetchAttempts++;
-			if (!responseData) await fetchModrinthProject(project, fetchAttempts);
-			return responseData;
-		}
-
 		for (const project of projects) {
 			const projectPlatform = project.project_id.match(/[A-z]/) ? 'modrinth' : 'curseforge';
 
@@ -50,13 +24,13 @@ module.exports = {
 			let fetchedProject = null;
 			switch (projectPlatform) {
 			case 'curseforge':
-				responseData = await fetchCurseForgeProject(project, 0);
+				responseData = await getMod(project.project_id, 5);
 				if (!responseData) continue;
 				fetchedProject = await getJSONResponse(responseData.body);
 				fetchedProject = fetchedProject.data;
 				break;
 			case 'modrinth':
-				responseData = await fetchModrinthProject(project, 0);
+				responseData = await getProject(project.project_id, 5);
 				if (!responseData) continue;
 				fetchedProject = await getJSONResponse(responseData.body);
 				break;
