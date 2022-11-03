b6efbe6029345c8391ba83a2a3fab3c4b65c57a7
beans-squared <starskillz13@gmail.com>
2022-11-02 22:47:22 -0500

    Add admin script for setting guild max tracked projects

---
+++ scripts/set-guild-max-tracking.js
@@ -0,0 +1,18 @@
+const readline = require("node:readline");
+const { Guilds } = require("../database/models");
+
+(async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("Enter guild ID: ", async (id) => {
+    const guild = await Guilds.findByPk(id);
+    rl.question("Enter new max: ", async (max) => {
+      await guild.setMaxTrackedProjects(max);
+      console.log(`Set guild's max tracked projects to ${max}.`);
+      rl.close();
+    });
+  });
+})();
