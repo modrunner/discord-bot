51826a1b31a30a7e6a077426acda1d6350e3e384
smcmo <starskillz13@gmail.com>
2022-12-20 13:54:12 -0600

    bye bash scripts

--- fetch-production-updates.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-echo "Pulling latest changes from the remote..."
-git pull
-
-echo "Restarting bot process..."
-pm2 restart modrunner-bot
-
-echo "Bot has been updated."
--- restart-staging.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-
-pm2 restart modrunner-staging
