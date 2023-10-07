03bbdb9e83f70f7da95d5769c27ba5bc0d798d13
smcmo <shanemcmonigal@gmail.com>
2023-10-07 10:33:41 -0500

    Added settings initialization for guilds not in database on using track

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,11 +4,12 @@ const logger = require('../logger');
 module.exports = {
   name: 'guildCreate',
   async execute(guild) {
+    logger.info(`Client was invited to guild ${guild.name} (${guild.id}).`);
+
     // Add settings to database for guild
     await Guilds.create({
       id: guild.id,
     });
-
-    logger.info(`Client was invited to guild ${guild.name} (${guild.id}). Initialized settings.`);
+    logger.info(`Initialized settings for guild ${guild.name} (${guild.id})`);
   },
 };
