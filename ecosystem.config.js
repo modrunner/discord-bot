bbbcfdfa5e2ffac67b16735461bae906c2002688
smcmo <shanemcmonigal@gmail.com>
2023-10-07 09:44:36 -0500

    I said fix pls

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
 			name: 'modrunner-bot',
 			script: 'npm run start',
 			watch: true,
-			ignore_watch: ['node_modules', 'discord-bot/database'],
+			ignore_watch: ['node_modules', 'discord-bot/database', 'modrunner-staging/database'],
 			min_uptime: '30s',
 			max_restarts: 5,
 		}
