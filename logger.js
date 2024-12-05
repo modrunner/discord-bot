2b917687c0c128bcf7f1c3ee682c502a43497a65
smcmo <shanemcmonigal@gmail.com>
2024-12-05 08:48:40 -0600

    Add logtail source

--- logger.js
+++ logger.js
@@ -3,11 +3,19 @@ const pino = require('pino');
 const logger = pino({
   level: process.env.LOGGING_LEVEL ?? 'info',
   transport: {
-    target: 'pino-pretty',
-    options: {
-      ignore: process.env.DOPPLER_ENVIRONMENT === 'dev' ? 'pid,hostname' : 'pid,hostname',
-      translateTime: 'SYS:yyyy-mm-dd hh:MM:s:l TT',
-    },
+    targets: [
+      {
+        target: 'pino-pretty',
+        options: {
+          ignore: process.env.DOPPLER_ENVIRONMENT === 'dev' ? 'pid,hostname' : 'pid,hostname',
+          translateTime: 'SYS:yyyy-mm-dd hh:MM:s:l TT',
+        },
+      },
+      {
+        target: '@logtail/pino',
+        options: { sourceToken: process.env.BETTERSTACK_SOURCE_TOKEN },
+      },
+    ],
   },
 });
 
